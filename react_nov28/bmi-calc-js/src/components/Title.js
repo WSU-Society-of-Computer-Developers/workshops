b3def3ea713e2c93e58f36e292abfb6c65597ca3
Zavaar Shah <thatziv@gmail.com>
2022-11-28 21:39:24 -0500

    added js instructory code

---
+++ react_nov28/bmi-calc-js/src/components/Title.js
@@ -0,0 +1,7 @@
+import logo from '../logo.svg';
+export default function Title() {
+    return <div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3>BMI Calculator in React</h3>
+    </div>
+}
