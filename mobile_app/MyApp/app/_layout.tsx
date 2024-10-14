6c84058f732fbf63b84750a1f7e4c242b9f007c6
Zavaar Shah <thatziv@gmail.com>
2024-10-14 17:12:06 -0400

    started react native

---
+++ mobile_app/MyApp/app/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="counter" />
+    </Stack>
+  );
+}
---
+++ mobile_app/MyApp/app/index.tsx
@@ -0,0 +1,20 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link push href="/counter">
+        {" "}
+        Go to Counter
+      </Link>
+    </View>
+  );
+}
